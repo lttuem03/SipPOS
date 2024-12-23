fed60bf5ab7cbe50f67e800a35f226c577bc7982
duongtanton <ton.t.duong@pkh.vn>
2024-12-23 20:21:05 +0700

    FEATURE: Add inventory unit test.     - Product service unit test     - Category service unit test

---
+++ src/SipPOS.UnitTest/Services/Entity/Implementations/CategoryServiceUnitTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using SipPOS.DataTransfer.Entity;
+using SipPOS.Services.Entity.Implementations;
+
+namespace SipPOS.UnitTest.Services.Entity.Implementations
+{
+    [TestClass]
+    public class CategoryServiceUnitTest
+    {
+
+        public CategoryService categoryService = App.GetService<CategoryService>();
+
+
+        [TestMethod]
+        public void GetCategoryById_WhenCategoryExists_ShouldReturnCategory()
+        {
+            var categoryId = 1;
+
+            var category = categoryService.GetById(categoryId);
+
+            Assert.IsNotNull(category);
+            Assert.AreEqual(categoryId, category.Id);
+        }
+
+        [TestMethod]
+        public void GetCategoryById_WhenCategoryDoesNotExist_ShouldReturnNull()
+        {
+            var categoryId = -1;
+
+            var category = categoryService.GetById(categoryId);
+
+            Assert.IsNull(category);
+        }
+
+        [TestMethod]
+        public void GetCategorys_WhenCalled_ShouldReturnCategoryList()
+        {
+
+            var categorys = categoryService.GetAll();
+
+            Assert.IsNotNull(categorys);
+            Assert.IsTrue(categorys.Count != 0);
+
+        }
+
+        [TestMethod]
+        public void DeleteCategory_WhenCategoryExists_ShouldDeleteCategory()
+        {
+            var categoryId = 1;
+
+            var category = categoryService.DeleteById(categoryId);
+
+            Assert.IsNotNull(category.DeletedAt);
+        }
+
+        [TestMethod]
+        public void DeleteCategory_WhenCategoryDoesNotExist_ShouldNotDeleteCategory()
+        {
+            var categoryId = -1;
+
+            var category = categoryService.DeleteById(categoryId);
+
+            Assert.IsNull(category);
+        }
+
+        [TestMethod]
+        public void AddCategory_WhenCategoryIsValid_ShouldAddCategory()
+        {
+            var category = new CategoryDto
+            {
+                Name = "Test Category",
+                Desc = "Test Description"
+            };
+
+            var categoryDto = categoryService.Insert(category);
+
+            Assert.IsNotNull(categoryDto);
+            Assert.AreEqual(category.Name, categoryDto.Name);
+            Assert.AreEqual(category.Desc, categoryDto.Desc);
+
+            categoryService.DeleteById((long)category.Id);
+        }
+
+    }
+}
---
+++ src/SipPOS.UnitTest/Services/Entity/Implementations/ProductServiceUnitTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using SipPOS.DataTransfer.Entity;
+using SipPOS.Services.Entity.Implementations;
+
+namespace SipPOS.UnitTest.Services.Entity.Implementations
+{
+
+    [TestClass]
+    public class ProductServiceUnitTest
+    {
+        public ProductService productService = App.GetService<ProductService>();
+
+
+        [TestMethod]
+        public void GetProductById_WhenProductExists_ShouldReturnProduct()
+        {
+            var productId = 1;
+
+            var product = productService.GetById(productId);
+
+            Assert.IsNotNull(product);
+            Assert.AreEqual(productId, product.Id);
+        }
+
+        [TestMethod]
+        public void GetProductById_WhenProductDoesNotExist_ShouldReturnNull()
+        {
+            var productId = -1;
+
+            var product = productService.GetById(productId);
+
+            Assert.IsNull(product);
+        }
+
+        [TestMethod]
+        public void GetProducts_WhenCalled_ShouldReturnProductList()
+        {
+
+            var products = productService.GetAll();
+
+            Assert.IsNotNull(products);
+            Assert.IsTrue(products.Count != 0);
+
+        }
+
+        [TestMethod]
+        public void DeleteProduct_WhenProductExists_ShouldDeleteProduct()
+        {
+            var productId = 1;
+
+            var product = productService.DeleteById(productId);
+
+            Assert.IsNotNull(product.DeletedAt);
+        }
+
+        [TestMethod]
+        public void DeleteProduct_WhenProductDoesNotExist_ShouldNotDeleteProduct()
+        {
+            var productId = -1;
+
+            var product = productService.DeleteById(productId);
+
+            Assert.IsNull(product);
+        }
+
+        [TestMethod]
+        public void AddProduct_WhenProductIsValid_ShouldAddProduct()
+        {
+            var product = new ProductDto
+            {
+                Name = "Test Product",
+                Desc = "Test Description",
+                Price = 10.0,
+                CategoryId = 1
+            };
+
+            var productDto = productService.Insert(product);
+
+            Assert.IsNotNull(productDto);
+            Assert.AreEqual(product.Name, productDto.Name);
+            Assert.AreEqual(product.Desc, productDto.Desc);
+
+            productService.DeleteById((long)product.Id);
+        }
+
+    }
+}
